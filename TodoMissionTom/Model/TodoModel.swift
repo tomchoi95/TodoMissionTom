d831a157d03317874580508eacc5fb93ee2c2907
Tom Bum Su Choi <qjatn0545@naver.com>
2025-01-23 09:41:59 +0900

    데이터 모델링 // 모델에 필요한 것. ID, title, content, initialDate, isCompleted

--- TodoMissionTom/Model/TodoModel.swift
+++ TodoMissionTom/Model/TodoModel.swift
@@ -5,13 +5,22 @@
 //  Created by 최범수 on 2025-01-20.
 //
 
+
 import SwiftUI
 import SwiftData
 
 @Model
 final class Todo {
+    var id: UUID = UUID()
     var title: String
-    init(title: String) {
+    var content: String
+    var initialDate: Date
+    var isCompleted: Bool
+    
+    init(title: String, content: String, initialDate: Date, isCompleted: Bool) {
         self.title = title
+        self.content = content
+        self.initialDate = initialDate
+        self.isCompleted = isCompleted
     }
 }
