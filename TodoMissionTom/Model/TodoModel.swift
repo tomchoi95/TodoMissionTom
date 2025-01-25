06369040c5fd7ec10efdd9e7c373785dbbe1a995
Tom Bum Su Choi <qjatn0545@naver.com>
2025-01-25 19:53:17 +0900

    빌어먹을 오류 발생

--- TodoMissionTom/Model/TodoModel.swift
+++ TodoMissionTom/Model/TodoModel.swift
@@ -10,23 +10,85 @@
 import SwiftUI
 import SwiftData
 
-enum SchemaVersion1
+typealias Todo = SchemaVersion1_0_1.Todo
 
-@Model
-final class Todo {
-    @Attribute(.unique) var id: UUID = UUID()
-    var title: String
-    var content: String
-    var initialDate: Date
-    var isCompleted: Bool
+enum MigrationPlan: SchemaMigrationPlan {
+    static var schemas: [any VersionedSchema.Type] {
+        [SchemaVersion1.self, SchemaVersion1_0_1.self]
+    }
+    static var stages: [MigrationStage] {
+        [SchemaVersion1ToSchemaVersion1_0_1]
+    }
+    static let SchemaVersion1ToSchemaVersion1_0_1: MigrationStage = .lightweight(fromVersion: SchemaVersion1.self, toVersion: SchemaVersion1_0_1.self)
     
-    init(title: String, content: String, initialDate: Date, isCompleted: Bool) {
-        self.title = title
-        self.content = content
-        self.initialDate = initialDate
-        self.isCompleted = isCompleted
+}
+
+enum SchemaVersion1: VersionedSchema {
+    static var models: [any PersistentModel.Type] {
+        [Todo.self]
     }
     
+    static var versionIdentifier: Schema.Version = .init(1, 0, 0)
+    
+    @Model
+    final class Todo {
+        @Attribute(.unique) var id: UUID = UUID()
+        var title: String
+        var content: String
+        var initialDate: Date
+        var isCompleted: Bool
+        
+        init(title: String, content: String, initialDate: Date, isCompleted: Bool) {
+            self.title = title
+            self.content = content
+            self.initialDate = initialDate
+            self.isCompleted = isCompleted
+        }
+        
+        
+    }
+
+}
+enum SchemaVersion1_0_1: VersionedSchema {
+    static var models: [any PersistentModel.Type] {
+        [Todo.self]
+    }
+    
+    static var versionIdentifier: Schema.Version = .init(1, 0, 1)
+    
+    @Model
+    final class Todo {
+        @Attribute(.unique) var id: UUID = UUID()
+        var title: String
+        var content: String
+        @Attribute(originalName: "initialDate")
+        var initializedDate: Date
+        var isCompleted: Bool
+        var priority: Priority = Priority.medium
+        
+        init(title: String, content: String, initializedDate: Date, isCompleted: Bool) {
+            self.title = title
+            self.content = content
+            self.initializedDate = initializedDate
+            self.isCompleted = isCompleted
+        }
+        
+        
+    }
+
+}
+
+enum Priority: String, Codable {
+    case high = "🔴"
+    case medium = "🟡"
+    case low = "🟢"
+}
+
+
+
+
+
+extension Todo {
     static func predicate(searchText: String) -> Predicate<Todo> {
         return #Predicate<Todo> { todo in
             (searchText.isEmpty || todo.title.localizedStandardContains(searchText) || todo.content.localizedStandardContains(searchText))
